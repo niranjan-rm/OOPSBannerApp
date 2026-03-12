b8ffe222ac75c9b9790da5f106f9d500381c41c6
fa1478-007 <fa1478@srmist.edu.in>
2026-03-12 21:44:37 +0530

    Level1_q1

---
+++ DivisibilityChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class DivisibilityChecker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        
+        if (number % 5 == 0) {
+            System.out.println("Is the number " + number + " divisible by 5? Yes");
+        } else {
+            System.out.println("Is the number " + number + " divisible by 5? No");
+        }
+        
+        input.close();
+    }
+}
